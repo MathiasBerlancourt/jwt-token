5d8b25821ec2b5332135d9ce5b09b32861986e19
MathiasB <37438935+BerlancourtMathias@users.noreply.github.com>
2024-02-17 12:41:40 +0100

    set express server

--- jwtGenerator.js
+++ jwtGenerator.js
@@ -1,3 +1,4 @@
+const express = require("express");
 const jwt = require("jsonwebtoken");
 const axios = require("axios");
 const createAccessToken = require("./utils/accesTokenRequest");
@@ -5,20 +6,35 @@ require("dotenv").config();
 
 const private_key = process.env.DOCUSIGN_PRIVATE_KEY;
 
-// Définir les informations du jeton (payload)
-const payload = {
-  iss: "b51cc32a-8fde-4611-99dd-f5d4f6c50c75", // Clé d'intégration DocuSign
-  sub: "5469a366-a907-4faa-b2fe-6ab38089e6c7", // Identifiant de l'utilisateur de DocuSign
-  iat: Math.floor(Date.now() / 1000),
-  exp: Math.floor(Date.now() / 1000) + 6000,
-  aud: "account-d.docusign.com",
-  scope: "signature impersonation",
-};
-console.log("payload:", payload);
+const app = express();
+app.use(express.json());
 
-// Générer le token JWT
-const token = jwt.sign(payload, private_key, { algorithm: "RS256" });
+app.post("/accesstoken", async (req, res) => {
+  const user_id = req.body.user_id;
+  // Définir les informations du jeton (payload)
+  console.log("user_id :", user_id);
+  const payload = {
+    iss: "b51cc32a-8fde-4611-99dd-f5d4f6c50c75", // Clé d'intégration DocuSign
+    sub: user_id, // Identifiant de l'utilisateur de DocuSign "5469a366-a907-4faa-b2fe-6ab38089e6c7"
+    iat: Math.floor(Date.now() / 1000),
+    exp: Math.floor(Date.now() / 1000) + 6000,
+    aud: "account-d.docusign.com",
+    scope: "signature impersonation",
+  };
+  console.log("payload:", payload);
 
-console.log(token);
+  // Générer le token JWT
+  const token = jwt.sign(payload, private_key, { algorithm: "RS256" });
 
-createAccessToken(token);
+  try {
+    const response = await createAccessToken(token);
+
+    res.json(response);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
